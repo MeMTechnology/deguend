6d097e86b8aaa74e80e9ee2294e2b8ce5b541c63
matheusvieiraa <matheus_lemes1996@hotmail.com>
2017-09-22 17:04:00 -0300

    Exibindo mapa

--- src/pages/circuito/circuito.ts
+++ src/pages/circuito/circuito.ts
@@ -1,5 +1,7 @@
 import { NavController, NavParams } from 'ionic-angular';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+declare var google;
 
 @Component({
   selector: 'page-circuito',
@@ -7,11 +9,54 @@ import { Component } from '@angular/core';
 })
 export class CircuitoPage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad Circuito');
+    this.loadMap();
   }
+
+  loadMap(){
+    
+       let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+    
+       let mapOptions = {
+         center: latLng,
+         zoom: 15,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+       }
+    
+       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    
+     }
+
+    addMarker(){
+      
+       let marker = new google.maps.Marker({
+         map: this.map,
+         animation: google.maps.Animation.DROP,
+         position: this.map.getCenter()
+       });
+      
+       let content = "<h4>Information!</h4>";          
+      
+       this.addInfoWindow(marker, content);
+      
+     }
+
+     addInfoWindow(marker, content){
+      
+       let infoWindow = new google.maps.InfoWindow({
+         content: content
+       });
+      
+       google.maps.event.addListener(marker, 'click', () => {
+         infoWindow.open(this.map, marker);
+       });
+      
+     }
 
 }
