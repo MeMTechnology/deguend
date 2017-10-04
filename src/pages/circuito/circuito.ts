d4dae743038a064ed409bfb2da4a5bed79c3a3f1
Mateus <seraomateus@hotmail.com>
2017-10-03 22:07:17 -0300

    mostrando a rota no mapa

--- src/pages/circuito/circuito.ts
+++ src/pages/circuito/circuito.ts
@@ -11,9 +11,12 @@ declare var google;
 })
 export class CircuitoPage {
 
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay: any;
   codAgente: number;
   pontosRoute = "";
   @ViewChild('map') mapElement: ElementRef;
+  @ViewChild('directionsPanel') directionsPanel: ElementRef;
   map: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthService, private http:Http,) {
@@ -24,19 +27,62 @@ export class CircuitoPage {
 
   ionViewDidLoad() {
     this.loadMap();
-    this.loadRoute();
+    this.getDataPoints();
   }
 
-  loadRoute(){
+  getDataPoints(){
     this.http.get('http://localhost:8080/getRouteByAgente/'+this.codAgente).map(res => res.json())
     .subscribe(result => {
     this.pontosRoute = result;
-    console.log("Result: "+JSON.stringify(this.pontosRoute));
+    //console.log("Result: "+JSON.stringify(this.pontosRoute));
+    this.loadRoute(result);
     });
   }
 
-  loadMap(){
+  pontosInter(x){
+    var intermediarios = [];
+    for(var i = 1; i<x.length - 1; i++){
+      intermediarios.push(x[i]);
+    }
+    return intermediarios;
+  }
+
+  loadRoute(pontos){
+    var myTest =JSON.parse(pontos[0].pontosRota);
+    //console.log("TESTE: "+JSON.stringify(myTest));
+    this.geraRotaByPoints(myTest);
+  }
+
+  geraRotaByPoints(myTest){
+		var enderecoPartida = myTest[0].location;
+		var enderecoChegada = myTest[myTest.length - 1].location;
     
+    let directionsService = new google.maps.DirectionsService;
+    let directionsDisplay = new google.maps.DirectionsRenderer;
+
+  directionsDisplay.setMap(this.map);
+  //Essa linha abaixo é q tava dando pau.
+  //directionsDisplay.setPanel(this.directionsPanel.nativeElement);
+
+  directionsService.route({
+      origin: enderecoPartida,
+      destination: enderecoChegada,
+      waypoints: this.pontosInter(myTest),
+      travelMode: google.maps.TravelMode.DRIVING
+  }, (res, status) => {
+
+      if(status == google.maps.DirectionsStatus.OK){
+          directionsDisplay.setDirections(res);
+      } else {
+          console.warn(status);
+      }
+
+  });
+
+	//cleanMarcadores();//Testando...
+}
+
+  loadMap(){
        let latLng = new google.maps.LatLng(-22.65918, -45.8532757847999);
     
        let mapOptions = {
@@ -45,7 +91,12 @@ export class CircuitoPage {
          mapTypeId: google.maps.MapTypeId.ROADMAP
        }
     
-       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+       //this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+       this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+       this.directionsDisplay = new google.maps.DirectionsRenderer({
+        map: this.map
+    });
+       this.directionsDisplay.setMap(this.map);
     
      }
 
